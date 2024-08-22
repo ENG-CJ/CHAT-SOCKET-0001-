6c62b1afc9393a51795e832cd3d1eb6b5c33fb31
amohamud <cj@gmail.com>
2024-08-22 12:09:42 +0300

    Use environment port for WebSocket server

--- server.js
+++ server.js
@@ -59,4 +59,4 @@ wss.on('connection', ws => {
     });
 });
 
-console.log('WebSocket server is running on ws://localhost:8080');
+console.log('WebSocket server is running on '+process.env.PORT || 8080);
